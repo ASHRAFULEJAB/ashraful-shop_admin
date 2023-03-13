6e89317d0fa2c86f657653b8dea091eabf988cb9
Ashraful Islam <19103189@iubat.edu>
2023-03-14 02:25:25 +0600

    done-project

--- src/components/AddFoodData.js
+++ src/components/AddFoodData.js
@@ -87,27 +87,27 @@ const AddFoodData = () => {
   return (
     <>
       <Navbar />
-      <div className='food-outermost'>
+      <div className="food-outermost">
         {/* <Navbar /> */}
-        <div className='form-outer'>
-          <h2 className='admin'>Admin Pannel For Eat Now</h2>
-          <h1>Add Food Data</h1>
-          <form className='form-inner'>
-            <label>Food Name</label>
+        <div className="form-outer">
+          <h2 className="admin">Admin Pannel For Ashraful Shop</h2>
+          <h1>Add Product Data</h1>
+          <form className="form-inner">
+            <label>Product Name</label>
             <input
-              type='text'
-              name='food_name'
+              type="text"
+              name="food_name"
               onChange={(e) => {
-                setFoodName(e.target.value)
+                setFoodName(e.target.value);
               }}
             />
             <br />
-            <label>Food Description</label>
+            <label>Product Description</label>
             <input
-              type='text'
-              name='food_description'
+              type="text"
+              name="food_description"
               onChange={(e) => {
-                setFoodDescription(e.target.value)
+                setFoodDescription(e.target.value);
               }}
             />
             <br />
@@ -116,85 +116,85 @@ const AddFoodData = () => {
             {/*  */}
             {/*  */}
 
-            <div className='form-row'>
-              <div className='form-col'>
-                <label>Food Price</label>
+            <div className="form-row">
+              <div className="form-col">
+                <label>Product Price</label>
                 <input
-                  type='number'
-                  name='food_price'
+                  type="number"
+                  name="food_price"
                   onChange={(e) => {
-                    setFoodPrice(e.target.value)
+                    setFoodPrice(e.target.value);
                   }}
                 />
               </div>
-              <div className='form-col'>
-                <label>Food Type</label>
+              <div className="form-col">
+                <label>Product Type</label>
 
                 <select
-                  name='food_type'
+                  name="food_type"
                   onChange={(e) => {
-                    setFoodType(e.target.value)
+                    setFoodType(e.target.value);
                   }}
                 >
-                  <option value='null'>Select Food Type</option>
-                  <option value='veg'>Veg</option>
-                  <option value='non-veg'>Non-Veg</option>
+                  <option value="null">Select Product Type</option>
+                  <option value="veg">Veg</option>
+                  <option value="non-veg">Non-Veg</option>
                 </select>
               </div>
             </div>
-            <div className='form-row'>
-              <div className='form-col'>
-                <label>Food Category</label>
+            <div className="form-row">
+              <div className="form-col">
+                <label>Product Category</label>
                 <select
-                  name='food_category'
+                  name="food_category"
                   onChange={(e) => {
-                    setFoodCategory(e.target.value)
+                    setFoodCategory(e.target.value);
                   }}
                 >
-                  <option value='null'>Select Food Category</option>
-                  <option value='veg'>Indian</option>
-                  <option value='non-veg'>Chineese</option>
-                  <option value='non-veg'>Italian</option>
-                  <option value='non-veg'>Mexican</option>
-                  <option value='non-veg'>American</option>
+                  <option value="null">Select Product Category</option>
+                  <option value="veg">Indian</option>
+                  <option value="non-veg">Chineese</option>
+                  <option value="non-veg">Italian</option>
+                  <option value="non-veg">Mexican</option>
+                  <option value="non-veg">American</option>
                 </select>
               </div>
-              <div className='form-col'>
-                <label>Meal Type</label>
+              <div className="form-col">
+                <label>Product Type</label>
 
                 <select
-                  name='meal_type'
+                  name="meal_type"
                   onChange={(e) => {
-                    setMealType(e.target.value)
+                    setMealType(e.target.value);
                   }}
                 >
-                  <option value='null'>Select Meal Type</option>
-                  <option value='dinner'>Dinner</option>
-                  <option value='staters'>Starters</option>
-                  <option value='breakfast'>Breakfast</option>
-                  <option value='liquid'>Liquid</option>
+                  <option value="null">Select Product Type</option>
+                  <option value="dinner">Dinner</option>
+                  <option value="staters">Starters</option>
+                  <option value="breakfast">Breakfast</option>
+                  <option value="liquid">Liquid</option>
                 </select>
               </div>
             </div>
             <br />
-            <div className='form-row'>
-              <div className='form-col'>
+            <div className="form-row">
+              <div className="form-col">
                 <label>Add On</label>
                 <input
-                  type='text'
-                  name='food_addon'
+                  type="text"
+                  name="food_addon"
                   onChange={(e) => {
-                    setFoodAddon(e.target.value)
+                    setFoodAddon(e.target.value);
                   }}
                 />
               </div>
-              <div className='form-col'>
+              <div className="form-col">
                 <label>Add On Price</label>
                 <input
-                  type='text'
-                  name='food_addon_price'
+                  type="text"
+                  name="food_addon_price"
                   onChange={(e) => {
-                    setFoodAddonPrice(e.target.value)
+                    setFoodAddonPrice(e.target.value);
                   }}
                 />
               </div>
@@ -203,99 +203,99 @@ const AddFoodData = () => {
             {/*  */}
             {/*  */}
             {/*                                          */}
-            <label>Food Image</label>
+            <label>Product Image</label>
             <input
-              type='file'
-              name='food_image'
+              type="file"
+              name="food_image"
               onChange={(e) => {
-                setFoodImage(e.target.files[0])
+                setFoodImage(e.target.files[0]);
               }}
             />
             <br />
-            <label>Restaurant Name</label>
+            <label>Product's Name</label>
             <input
-              type='text'
-              name='restaurant_name'
+              type="text"
+              name="restaurant_name"
               onChange={(e) => {
-                setRestaurantName(e.target.value)
+                setRestaurantName(e.target.value);
               }}
             />
             <br />
-            <div className='form-row'>
-              <div className='form-col'>
-                <label>Restaurant Building Number/Name</label>
+            <div className="form-row">
+              <div className="form-col">
+                <label>Product Identify Number/Name</label>
                 <input
-                  type='text'
-                  name='restaurant_address_building'
+                  type="text"
+                  name="restaurant_address_building"
                   onChange={(e) => {
-                    setRestrauntAddressBuilding(e.target.value)
+                    setRestrauntAddressBuilding(e.target.value);
                   }}
                 />
               </div>
-              <div className='form-col'>
-                <label>Restaurant Street / Area Name</label>
+              <div className="form-col">
+                <label>Product Street / Area Name</label>
                 <input
-                  type='text'
-                  name='restaurant_address_street'
+                  type="text"
+                  name="restaurant_address_street"
                   onChange={(e) => {
-                    setRestrauntAddressStreet(e.target.value)
+                    setRestrauntAddressStreet(e.target.value);
                   }}
                 />
               </div>
             </div>
 
-            <div className='form-row'>
-              <div className='form-col'>
-                <label>Restaurant City</label>
+            <div className="form-row">
+              <div className="form-col">
+                <label>Product City</label>
                 <input
-                  type='text'
-                  name='restaurant_address_city'
+                  type="text"
+                  name="restaurant_address_city"
                   onChange={(e) => {
-                    setRestrauntAddressCity(e.target.value)
+                    setRestrauntAddressCity(e.target.value);
                   }}
                 />
               </div>
-              <div className='form-col'>
-                <label>Restaurant Pin-code</label>
+              <div className="form-col">
+                <label>Product Pin-code</label>
                 <input
-                  type='number'
-                  name='restaurant_address_pincode'
+                  type="number"
+                  name="restaurant_address_pincode"
                   onChange={(e) => {
-                    setRestrauntAddressPincode(e.target.value)
+                    setRestrauntAddressPincode(e.target.value);
                   }}
                 />
               </div>
             </div>
 
-            <div className='form-row'>
-              <div className='form-col'>
-                <label>Restaurant Phone</label>
+            <div className="form-row">
+              <div className="form-col">
+                <label>Product Phone</label>
                 <input
-                  type='number'
-                  name='restaurant_phone'
+                  type="number"
+                  name="restaurant_phone"
                   onChange={(e) => {
-                    setRestaurantPhone(e.target.value)
+                    setRestaurantPhone(e.target.value);
                   }}
                 />
               </div>
-              <div className='form-col'>
-                <label>Restaurant Email</label>
+              <div className="form-col">
+                <label>Product Email</label>
                 <input
-                  type='email'
-                  name='restaurant_email'
+                  type="email"
+                  name="restaurant_email"
                   onChange={(e) => {
-                    setRestaurantEmail(e.target.value)
+                    setRestaurantEmail(e.target.value);
                   }}
                 />
               </div>
             </div>
             <br />
-            <button onClick={handleSubmit}>Add Food</button>
+            <button onClick={handleSubmit}>Add Product</button>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default AddFoodData
